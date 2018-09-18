5af71be3df35fd4099b3dd038c11c90752952be5
Divyansh Saxena <divyansh.stat@gmail.com>
2018-09-18 22:57:37 +0100

    convert the json to csv and fetch imp parameters
    After fetching the relevant data from the github api,
    
    select the important parameters and create a csv of output object

---
+++ visualization/plot.py
@@ -0,0 +1,33 @@
+import json
+import os
+import csv
+import numpy as np
+
+json_file = open("/Users/div/Documents/GitHub/headstart_assignment/cleaned_issues_list.json")
+json_data = json.load(json_file)
+ 
+total_count = []
+plus_1 = []
+minus_1 = []
+confused = []
+heart = []
+hooray = []
+laugh = []
+title = []
+comments = []
+
+for i in xrange(1,len(json_data)):
+	total_count.append(json_data[i]['reactions']['total_count'])
+	plus_1.append(json_data[i]['reactions']['+1'])
+	minus_1.append(json_data[i]['reactions']['-1'])
+	confused.append(json_data[i]['reactions']['confused'])
+	heart.append(json_data[i]['reactions']['heart'])
+	hooray.append(json_data[i]['reactions']['hooray'])
+	laugh.append(json_data[i]['reactions']['laugh'])
+	title.append(json_data[i]['title'])
+	comments.append(json_data[i]['comments'])
+
+
+data = list(zip(title,comments,total_count,plus_1,minus_1,confused,heart,hooray,laugh))
+
+np.savetxt("/Users/div/Documents/GitHub/headstart_assignment/visualization/issues.csv", data, delimiter=",", fmt='%s')
